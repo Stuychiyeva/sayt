df133bf31680364d5a440d6a162460bb682d14ff
Stuychiyeva <sarvaratuychiyeva2@gmail.com>
2025-07-26 11:24:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//alert("Hello world")
+
+//let a = 15
+
+//let age = 12
+//
+
+//console.log(age);
+//console.log(name);
+
+//alert(age);
+//alert(name);
+
+//var a = 10;
+//a = 11;
+//a = 1;
+//b = 14;
+//a = b;
+//alert(a)
+
+
+const b = 7;
+const a = 5;
+alert(b);
